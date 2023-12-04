d2dc311f559596b22957d748e667f95448571ca9
Manuel Stolze <manu.stolze93@yahoo.de>
2023-12-04 20:29:22 +0100

    chore: prettified.

--- 2023/day04/src/index.ts
+++ 2023/day04/src/index.ts
@@ -1,54 +1,50 @@
 import inputFile from "./input";
 import { Card } from "./Card";
-import {counting} from "radash";
-
+import { counting } from "radash";
 
 function parseFile() {
-    const data = inputFile.split('\n')
+  const data = inputFile.split("\n");
 
-    return data.map((line) => {
+  return data.map((line) => {
+    const [rawId, data] = line.split(": ");
+    const [winningNumberString, ourNumberString] = data.split(" | ");
 
-        const [rawId, data] = line.split(": ")
-        const [winningNumberString, ourNumberString] = data.split(' | ');
+    const id = Number(rawId.replace("Card ", ""));
+    const winningNumbers = extractNumbersFromString(winningNumberString);
+    const ourNumbers = extractNumbersFromString(ourNumberString);
 
-        const id = Number(rawId.replace("Card ", ""))
-        const winningNumbers = extractNumbersFromString(winningNumberString);
-        const ourNumbers = extractNumbersFromString(ourNumberString);
-
-        return new Card(id, winningNumbers, ourNumbers)
-    })
+    return new Card(id, winningNumbers, ourNumbers);
+  });
 }
 
 function extractNumbersFromString(inputString: string) {
-    const numbers = inputString.match(/\d+/g);
+  const numbers = inputString.match(/\d+/g);
 
-    if (!numbers) { return []; }
+  if (!numbers) {
+    return [];
+  }
 
-    return numbers.map(Number);
+  return numbers.map(Number);
 }
-
 
 const part1 = () => {
-    return parseFile()
-        .map(card => card.sumOfPoints)
-        .reduce((acc, value) =>  acc + value, 0)
-}
+  return parseFile()
+    .map((card) => card.sumOfPoints)
+    .reduce((acc, value) => acc + value, 0);
+};
 
 const part2 = () => {
-    const cards = parseFile();
-    const counter = counting(cards, card => card.id)
+  const cards = parseFile();
+  const counter = counting(cards, (card) => card.id);
 
-    cards.forEach(card => {
-        card.winningCopiesIds.forEach(id => {
-            counter[id] += counter[card.id]
-        })
-    })
+  cards.forEach((card) => {
+    card.winningCopiesIds.forEach((id) => {
+      counter[id] += counter[card.id];
+    });
+  });
 
-    return Object.values(counter).reduce((acc, count) => acc + count, 0)
-}
-
+  return Object.values(counter).reduce((acc, count) => acc + count, 0);
+};
 
 console.log(part1());
 console.log(part2());
-
-
